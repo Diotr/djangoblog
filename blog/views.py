11e6b5d19d53dd17d09c5f9f9c699f635c745789
Diotr <PiotrDab30@outlook.com>
2024-08-30 12:40:16 +0200

    Kopia gotowca

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView
-from blog.forms import PostForm
-from django.views.generic import DetailView
-
-
-class Image(TemplateView):
-    form = PostForm
-    template_name = 'blog/image.html'
-
-    def post(self, request, *args, **kwargs):
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            obj = form.save()
-            return HttpResponseRedirect(reverse_lazy('image_display', kwargs={'pk': obj.id}))
-
-        context = self.get_context_data(form=form)
-        return self.render_to_response(context)
-
-    def get(self, request, *args, **kwargs):
-        return self.post(request, *args, **kwargs)
-
-class ImageDisplay(DetailView):
-    model = Post
-    template_name = 'blog/image_display.html'
-    context_object_name = 'image'
-
+from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -40,7 +13,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def error_404_view(request, exception):
-    data = {'name': "Blog dla programistów"}
+    data = {"name": 'Blog dla programistów'}
     return render(request, 'blog/404.html', data)
 
 def post_new(request):
@@ -49,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.publish_date = timezone.now()
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -63,9 +36,13 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.publish_date = timezone.now()
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mysite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,21 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog.views import Image, ImageDisplay
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('image/', Image.as_view(), name='image'),
-    path('image/<int:pk>/', ImageDisplay.as_view(), name='image_display'),
-]
 
+]
 handler404 = 'blog.views.error_404_view'
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
